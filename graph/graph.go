c532b54fb9d91947c6b0be361af22123501476f8
bruteforce1414 <bruteforce1414@gmail.com>
2018-12-13 19:31:28 +0500

    Все функции работают. Но мне нужно усложнить тест!

--- graph/graph.go
+++ graph/graph.go
@@ -7,23 +7,19 @@ import (
 
 //узел
 type node struct {
-	id int
+	id   int
 	name string
 }
-
 
 //узел
 type Node interface {
 	Id() int
 	Name() string
-
 }
 
-
-
 type edge struct {
-	from Node
-	to Node
+	from   Node
+	to     Node
 	weight float64
 }
 
@@ -34,17 +30,15 @@ type Edge interface {
 	Weight() float64
 }
 
-
 type DirectedGraph struct {
-nodes 	map[int]Node
-from  	map[int]map[int]Edge
-to    	map[int]map[int]Edge
-lock sync.RWMutex
+	nodes map[int]Node
+	from  map[int]map[int]Edge
+	to    map[int]map[int]Edge
+	lock  sync.RWMutex
 }
 
 // это основные методы графа
 type Graph interface {
-
 
 	// проверка существует ли такая нода
 	Has(Node) bool
@@ -67,6 +61,8 @@ type Graph interface {
 	// добавить ноду (без ребер)
 	AddNode(n Node)
 
+	AddNodeThroughSetEdge(n Node)
+
 	// удалить ноду
 	RemoveNode(n Node)
 
@@ -76,212 +72,237 @@ type Graph interface {
 	// удалить ребро
 	RemoveEdge(e Edge)
 }
+
 // + проверка существует ли такая нода
-func (g *DirectedGraph) Has(n Node) bool  {
+func (g *DirectedGraph) Has(n Node) bool {
 	if _, ok := g.nodes[n.Id()]; ok {
 		return true
 	}
 	return false
 }
+
 // + возвращаем просто все ноды без какого либа разбора-сортировки
-func (g *DirectedGraph) Nodes() map[int]Node{
+func (g *DirectedGraph) Nodes() map[int]Node {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
 	return g.nodes
 }
 
 //+ возвращаем список нод достижимых из заданной ноды
-func (g *DirectedGraph) From(n Node) []Node{
+func (g *DirectedGraph) From(n Node) []Node {
 	g.lock.RLock()
 	defer g.lock.RUnlock()
 	var returnedNodes []Node
-	if _, ok := g.nodes[n.Id()];ok{
-		for _,value1:=range g.from[n.Id()]{
-			returnedNodes=append(returnedNodes,value1.To())
+	if _, ok := g.nodes[n.Id()]; ok {
+		for _, value1 := range g.from[n.Id()] {
+			returnedNodes = append(returnedNodes, value1.To())
 		}
 		return returnedNodes
 	}
 
-return nil
+	return nil
 }
 
 // все ноды из которых можно достич указанную
-func (g *DirectedGraph) To(n Node) []Node{
+func (g *DirectedGraph) To(n Node) []Node {
 	g.lock.RLock()
 	defer g.lock.RUnlock()
 	var returnedNodes []Node
-	if _, ok := g.nodes[n.Id()];ok{
-		for _,value:=range g.to[n.Id()]{
-			returnedNodes=append(returnedNodes,value.From())
+	if _, ok := g.nodes[n.Id()]; ok {
+		for _, value := range g.to[n.Id()] {
+			returnedNodes = append(returnedNodes, value.From())
 		}
 		return returnedNodes
 	}
 	return nil
 }
+
 //+ проверка есть ли ребро из u в v
-func (g *DirectedGraph) HasEdgeFromTo(u, v Node) bool{
+func (g *DirectedGraph) HasEdgeFromTo(u, v Node) bool {
 	if _, ok := g.from[u.Id()][v.Id()]; ok {
 		return true
-
 	}
-
+	/*
+		if _, ok := g.to[u.Id()][v.Id()]; ok {
+			return true
+		}
+	*/
 	return false
 }
 
 //+возвращает ребро из u и v, если такое ребро существует, иначе nil
-func (g *DirectedGraph) Edge(u, v Node) Edge  {
+func (g *DirectedGraph) Edge(u, v Node) Edge {
 	if _, ok := g.from[u.Id()][v.Id()]; ok {
-	return g.from[u.Id()][v.Id()]
-		}
-
+		return g.from[u.Id()][v.Id()]
+	}
 
 	return nil
 }
 
 //+добавить ноду (без ребер)
 func (g *DirectedGraph) AddNode(n Node) {
-	//g.lock.Lock()
+	g.lock.Lock()
 	//проверим, есть ли нода с таким же идентификатором в графе
-		if g.Has(n){
-			fmt.Println("Вершина с идентификатором '", n.Id(),"' уже существует. Вершина не была добавлена к графу")
-			return
-		}
-	g.nodes[n.Id()]=n
-//	g.lock.Unlock()
+	if g.Has(n) {
+		fmt.Println("Вершина с идентификатором '", n.Id(), "' уже существует. Вершина не была добавлена к графу")
+		return
+	}
+	g.nodes[n.Id()] = n
+	g.lock.Unlock()
 }
+
+func (g *DirectedGraph) AddNodeThroughSetEdge(n Node) {
+	//проверим, есть ли нода с таким же идентификатором в графе
+	if g.Has(n) {
+		fmt.Println("Вершина с идентификатором '", n.Id(), "' уже существует. Вершина не была добавлена к графу")
+		return
+	}
+	g.nodes[n.Id()] = n
+
+}
+
 //+ удалить ноду
 func (g *DirectedGraph) RemoveNode(n Node) {
 	g.lock.Lock()
 	//проверим, есть ли нода с таким же идентификатором в графе
-	if g.Has(n)==false{
-		fmt.Println("Вершина с идентификатором '", n.Id(),"' не существует. Невозможно удалить несуществующую вершину")
+	if g.Has(n) == false {
+		fmt.Println("Вершина с идентификатором '", n.Id(), "' не существует. Невозможно удалить несуществующую вершину")
 		return
 	}
 
 	//удаляем все рёбра связанные с вершиной
 
-	for _,value:=range g.nodes {
-		if g.HasEdgeFromTo(n,value){
-			delete(g.from,value.Id())
+	for _, value := range g.nodes {
+		if g.HasEdgeFromTo(n, value) {
+			delete(g.from, n.Id())
 		}
-
-	}
-
-	for _,value:=range g.nodes {
-		if g.HasEdgeFromTo(value,n){
-			delete(g.to,value.Id())
+		if g.HasEdgeFromTo(value, n) {
+			delete(g.from[value.Id()], n.Id())
+			if len(g.from[value.Id()]) == 0 {
+				delete(g.from, n.Id())
+			}
 		}
-
 	}
 
+	for _, value := range g.nodes {
+		if _, ok := g.to[n.Id()][value.Id()]; ok {
+			{
+				delete(g.to, n.Id())
+			}
 
 
 
-	if _, ok := g.to[n.Id()]; ok {
-		delete(g.to, n.Id())
+		}
+		if _, ok := g.to[value.Id()][n.Id()]; ok {
+			{
+				delete(g.to[value.Id()], n.Id())
+				if len(g.to[value.Id()]) == 0 {
+					delete(g.to, n.Id())
+				}
+			}
+		}
 	}
+
 	//удаляем вершину
 	var indexDelete int
-	for _,value:=range g.nodes{
-		if value.Id() == n.Id(){
-			indexDelete=n.Id()
+	for _, value := range g.nodes {
+		if value.Id() == n.Id() {
+			indexDelete = n.Id()
 			break
 		}
 	}
 	delete(g.nodes, indexDelete)
 
-
-
-
 	g.lock.Unlock()
 
 }
+
 // добавить ребро
-func (g *DirectedGraph)SetEdge(e Edge)  {
+func (g *DirectedGraph) SetEdge(e Edge) {
 	g.lock.Lock()
-	if g.HasEdgeFromTo(e.From(),e.To()){
-		fmt.Println("Дуга с началом в вершине '", e.From(),"' до вершины '", e.To(),"' " +
-			"уже существует. Дуга не была добавлена к графу. Созданы вершины '", e.From()," и '", e.To(),"'")
+	if g.HasEdgeFromTo(e.From(), e.To()) {
+		fmt.Println("Дуга с началом в вершине '", e.From(), "' до вершины '", e.To(), "' "+
+			"уже существует. Дуга не была добавлена к графу. Созданы вершины '", e.From(), " и '", e.To(), "'")
 		return
 	}
 
-		if !g.Has(e.From()){
-			g.AddNode(e.From())
-		}
-		if !g.Has(e.To()){
-			g.AddNode(e.To())
-		}
-	
+	if !g.Has(e.From()) {
+		g.AddNodeThroughSetEdge(e.From())
+	}
+	if !g.Has(e.To()) {
+		g.AddNodeThroughSetEdge(e.To())
+	}
+
 	//Если первого ключа не существует, то создаём новый массив для вторых ключей
 	//Добавление в список дуг ИЗ
 	if _, ok := g.from[e.From().Id()]; !ok {
 		g.from[e.From().Id()] = map[int]Edge{}
 		g.from[e.From().Id()][e.To().Id()] = e
-	} else{
+	} else {
 		////Если первый ключ уже есть, то добавляем к уже существующему массиву для вторых ключей
 		//Добавление в список дуг ИЗ
 		g.from[e.From().Id()][e.To().Id()] = e
 	}
 
 	//Добавление в список дуг В
-	if _, ok := g.to[e.From().Id()]; !ok {
+	if _, ok := g.to[e.To().Id()]; !ok {
 		g.to[e.To().Id()] = map[int]Edge{}
 		g.to[e.To().Id()][e.From().Id()] = e
-	}else {
-	//Добавление в список дуг В
-	g.to[e.To().Id()][e.From().Id()] = e
+	} else {
+		//Добавление в список дуг В
+		g.to[e.To().Id()][e.From().Id()] = e
 	}
 
-
-  		g.lock.Unlock()
+	g.lock.Unlock()
 
 }
+
 //+ удалить ребро
 func (g *DirectedGraph) RemoveEdge(e Edge) {
 	g.lock.Lock()
-	if !g.HasEdgeFromTo(e.From(),e.To()){
-		fmt.Println("Дуга с началом в вершине '", e.From(),"' до вершины '", e.To(),"' " +
+	if !g.HasEdgeFromTo(e.From(), e.To()) {
+		fmt.Println("Дуга с началом в вершине '", e.From(), "' до вершины '", e.To(), "' "+
 			"не существует. Невозможно удалить несуществующую дугу")
 		return
 	}
 	//удаляём второй ключ
-		delete(g.from[e.From().Id()], e.To().Id())
+	delete(g.from[e.From().Id()], e.To().Id())
 	//проверяем длину первой карты, если в ней больше нет элементов, то удаляем первичный ключ
-	if len(g.from[e.From().Id()])==0 {
+	if len(g.from[e.From().Id()]) == 0 {
 		delete(g.from, e.From().Id())
 	}
 
 	//удаляём второй ключ
 	delete(g.to[e.To().Id()], e.From().Id())
 	//проверяем длину первой карты, если в ней больше нет элементов, то удаляем первичный ключ
-	if len(g.to[e.To().Id()])==0 {
+	if len(g.to[e.To().Id()]) == 0 {
 		delete(g.to, e.To().Id())
 	}
-
 	g.lock.Unlock()
 }
 
 //* Получение значений инкапсулированных полей Ноды
 func (n *node) Id() int {
- 	return n.id
+	return n.id
 }
 func (n *node) Name() string {
 	return n.name
 }
 
 //* Получение значений инкапсулированных полей Дуги
-func (e *edge) From() Node{
+func (e *edge) From() Node {
 	return e.from
 }
-func (e *edge) To() Node{
+func (e *edge) To() Node {
 	return e.to
 }
 func (e *edge) Weight() float64 {
 	return e.weight
 }
 
-
 //создание нового графа
-func NewGraph() *DirectedGraph{
-	graphObject:=DirectedGraph{
+func NewGraph() *DirectedGraph {
+	graphObject := DirectedGraph{
 		map[int]Node{},
 		map[int]map[int]Edge{},
 		map[int]map[int]Edge{},
@@ -289,13 +310,15 @@ func NewGraph() *DirectedGraph{
 	}
 	return &graphObject
 }
+
 //создание нового узла
 func NewNode(id int, name string) Node {
-	nodeObject:=node{id:id,name:name}
+	nodeObject := node{id: id, name: name}
 	return &nodeObject
 }
+
 // создание новой дуги
 func NewEdge(from Node, to Node, weight float64) Edge {
-	edgeObject:=edge{from:from, to: to, weight:weight}
+	edgeObject := edge{from: from, to: to, weight: weight}
 	return &edgeObject
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -3,6 +3,8 @@ package graph_test
 import (
 	"github.com/bruteforce1414/queues/graph"
 	"github.com/stretchr/testify/assert"
+
+	//"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -11,11 +13,10 @@ func TestNewGraph(t *testing.T) {
 	a:=assert.New(t)
 	//create object graph without nodes and edges
 	testGraph := graph.NewGraph()
-	t.Log("Создан тестовый граф")
-
+	t.Log("Создан тестовый граф",testGraph)
 	//create node and add to graph
 	testNode1 := graph.NewNode(1, "Вершина1");testGraph.AddNode(testNode1)
-	a.Equal(true,testGraph.Has(testNode1))
+    a.Equal(true,testGraph.Has(testNode1))
 	testNode2 := graph.NewNode(2, "Вершина2");testGraph.AddNode(testNode2)
 	a.Equal(true,testGraph.Has(testNode2))
 	testNode3 := graph.NewNode(3, "Вершина3");testGraph.AddNode(testNode3)
@@ -23,32 +24,34 @@ func TestNewGraph(t *testing.T) {
 	testNode4 := graph.NewNode(4, "Вершина4");testGraph.AddNode(testNode4)
 	a.Equal(true,testGraph.Has(testNode4))
 
-	//testNode0:=graph.NewNode(0,"Несуществующая нода 0")
-	//testNode10:=graph.NewNode(10,"Несуществующая нода 10")
-	//testEdge01:=graph.NewEdge(testNode0,testNode1,0)
-	//testGraph.SetEdge(testEdge01)
-	//t.Log(testNode10)
+	testNode0:=graph.NewNode(0,"Несуществующая нода 0")
+	testNode10:=graph.NewNode(10,"Несуществующая нода 10")
+	testEdge01:=graph.NewEdge(testNode0,testNode1,0)
+	testEdge010:=graph.NewEdge(testNode0,testNode10,0)
+	testGraph.SetEdge(testEdge01)
+	testGraph.SetEdge(testEdge010)
+	t.Log(testNode10)
+
 	t.Log("Список вершин", testGraph.Nodes())
 	testEdge12 := graph.NewEdge(testNode1, testNode2, 12);testGraph.SetEdge(testEdge12)
 	a.Equal(true,testGraph.HasEdgeFromTo(testNode1,testNode2))
-	testEdge13 := graph.NewEdge(testNode1, testNode3, 13);testGraph.SetEdge(testEdge13)
+	testEdge21 := graph.NewEdge(testNode2, testNode1, 21);testGraph.SetEdge(testEdge21)
 	testEdge14 := graph.NewEdge(testNode1, testNode4, 14);testGraph.SetEdge(testEdge14)
 	testEdge34 := graph.NewEdge(testNode3, testNode4, 34);testGraph.SetEdge(testEdge34)
+	t.Log("Тестовый граф после добавления вершин", testGraph)
 	t.Log("Вершины, достижимые из вершины 1", testGraph.From(testNode1))
-  	 a.Equal(len(testGraph.From(testNode1)),3)
+	a.Equal(len(testGraph.From(testNode1)),2)
 	t.Log("Вершины, идущие к вершине 4.", testGraph.To(testNode4))
 	a.Equal(len(testGraph.To(testNode4)),2)
-  //  testGraph.RemoveEdge(testEdge12); t.Log("Список ребёр после удаления ребра 12", testGraph)
-  //	testGraph.RemoveEdge(testEdge13); t.Log("Список ребёр после удаления ребра 13", testGraph)
-
-	t.Log("Проверка наличия пути 13", testGraph.Edge(testNode1, testNode3))
-	t.Log("Проверка наличия пути 14", testGraph.Edge(testNode1, testNode4))
+    testGraph.RemoveEdge(testEdge12); t.Log("Список ребёр после удаления ребра 12", testGraph)
+	a.Equal(nil, testGraph.Edge(testNode1, testNode3))
+	a.Equal(true, testGraph.Edge(testNode1, testNode4))
 	t.Log("Проверка наличия пути 24", testGraph.Edge(testNode2, testNode4))
-	t.Log("Список вершин до удаления вершины 2   ", testGraph)
+	t.Log("Список вершин до удаления вершины 3  ", testGraph)
 	//Удаление вершины из списка вершин
-	testGraph.RemoveNode(testNode2);
-	t.Log("Список вершин после удаления вершины 2", testGraph)
-	//Попытка добавить уже существующую вершину
-	//testNode1double:=graph.NewNode(1,"Вершина 1");testGraph.AddNode(testNode1double)
+	testGraph.RemoveNode(testNode3);
+	t.Log("Список вершин после удаления вершины 3", testGraph)
+    //Попытка добавить уже существующую вершину
+	testNode1double:=graph.NewNode(1,"Вершина 1");testGraph.AddNode(testNode1double)
 
 }
